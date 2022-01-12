eeca37c6fac55d473fb1e1547bbac800f417239b
Osman Durdağ <40799665+zumrudu-anka@users.noreply.github.com>
2022-01-12 17:20:00 +0300

    Create Top.sql

---
+++ Basics/Top.sql
@@ -0,0 +1,7 @@
+SELECT TOP 100 * FROM Customers;
+
+SELECT TOP 1 PERCENT * FROM Customers;
+
+SELECT TOP 25 PERCENT ID, NAME FROM Customers;
+
+-- Ilk X satiri getirir, percent ile kullanimda tum satir sayisinin yuzde x i kadarini getirir.
