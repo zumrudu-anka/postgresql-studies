b54c477a349e036d9aa586a61fdf8c6fdf229f7b
Osman Durdağ <40799665+zumrudu-anka@users.noreply.github.com>
2022-01-12 16:14:10 +0300

    Update Between.sql

--- Basics/Between.sql
+++ Basics/Between.sql
@@ -1,1 +1,7 @@
 SELECT * FROM CUSTOMER WHERE AGE BETWEEN 22 AND 30;
+
+-- Yasi 22 ye esit ve buyuk olanlar ile 30 a esit ve kucuk olan musterileri getirir.
+
+-- Yani komut aslinda suna esittir
+
+SELECT * FROM CUSTOMER WHERE AGE >= 22 AND AGE <=30;
