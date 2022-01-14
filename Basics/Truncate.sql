32ef654d62923f87b6140dfe6e7d43524a9f4955
Osman Durdağ <40799665+zumrudu-anka@users.noreply.github.com>
2022-01-14 16:17:09 +0300

    Update Truncate.sql

--- Basics/Truncate.sql
+++ Basics/Truncate.sql
@@ -2,4 +2,7 @@
 
 -- Delete all records from the selected table
 
+-- Delete isleminden farki ise : Delete islemi ile tum satirlari silebiliriz. Fakat auto generated bir alan varsa kaldigi yerden devam eder.
+-- Fakat truncate islemi ile tum satirlari sildigimizde auto generated alanlar da sifirlanir. Yani bir id identity ise tekrar 0 dan baslar.
+
 Truncate table product;
