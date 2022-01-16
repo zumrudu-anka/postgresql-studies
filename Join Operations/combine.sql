f41b7f9eb3044e46d8d67277ceb19a6d676b3f84
Osman Durdağ <40799665+zumrudu-anka@users.noreply.github.com>
2022-01-16 22:40:00 +0300

    Update combine.sql

--- Join Operations/combine.sql
+++ Join Operations/combine.sql
@@ -1,4 +1,7 @@
 Select Users.*, Address.* from Users, Address Where Users.ID = Address.USER_ID;
 
 
-Select Users.*, Address.*, Country.* from Users, Address, City Where Users.ID = Address.USER_ID And City.ID = Address.Country_ID Group By Users.Username Having Users.ID = 5 Order By Users.ID;
+Select Users.*, Address.*, Country.* from Users, Address, City
+Where Users.ID = Address.USER_ID And City.ID = Address.Country_ID
+Group By Users.Username Having Users.ID = 5
+Order By Users.ID;
