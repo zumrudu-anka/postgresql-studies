e444c5c7293f4322c244561dd2dfdbc82acb924a
Osman Durdağ <40799665+zumrudu-anka@users.noreply.github.com>
2022-01-19 15:32:34 +0300

    Update substr.sql

--- StringOperations/substr.sql
+++ StringOperations/substr.sql
@@ -8,3 +8,16 @@ select substr('Osman Durdağ', 4, 6);
 select substring('Osman Durdağ', 4, 6);
 
 --> substr ve substring fonksiyonu tamamen aynidir.
+
+
+-- Good Example
+
+Select * from Users where username like 'O%'; -- Adi o ile baslayanlar
+
+Select * from Users where substring(username, 1, 1); -- Adi o ile baslayanlar
+
+-- Another Good Example
+
+Select * from Users where username like '%O'; -- Adi o ile bitenler
+
+Select * from Users where substring(username, length(username), 1); -- Adi o ile bitenler
