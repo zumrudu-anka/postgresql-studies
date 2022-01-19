f96259a5ff7ebf2439a14398e76cc995e4a5bf81
Osman Durdağ <40799665+zumrudu-anka@users.noreply.github.com>
2022-01-19 15:54:08 +0300

    Update and rename trim.sql to trim - ltrim - rtrim.sql

---
+++ StringOperations/trim - ltrim - rtrim.sql
@@ -0,0 +1,9 @@
+-- Sag ve soldaki bosluklari temizler
+
+SELECT TRIM('    SQL Tutorial    ') AS TrimmedString;
+
+-- Soldaki bosluklari temizler
+SELECT LTRIM('    SQL Tutorial    ') AS TrimmedString;
+
+-- Sagdaki bosluklari temizler
+SELECT RTRIM('    SQL Tutorial    ') AS TrimmedString;
--- StringOperations/trim.sql
+++
@@ -1,1 +0,0 @@
-SELECT TRIM('    SQL Tutorial    ') AS TrimmedString;
