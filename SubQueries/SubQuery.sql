d9eb940bbfd32c4116ee95e10f4277d02c9a3afd
Osman Durdağ <40799665+zumrudu-anka@users.noreply.github.com>
2022-01-19 14:52:12 +0300

    Update SubQuery.sql

--- SubQueries/SubQuery.sql
+++ SubQueries/SubQuery.sql
@@ -9,3 +9,7 @@
 -- update customer set balance = balance + balance * 0.1 where job = (select id from job where name = 'Engineer');
 
 select * from customer where city = 'ERZURUM' and job = (select id from job where name = 'Teacher');
+
+Select id, name, (select max(age) from users where city = "Erzurum") as maxAgedPersonInErzurum from customer;
+
+Select id, name as isim, (select max(age) from users where city = cs.cityId) as maxAgedPersonInErzurum from customer as cs;
